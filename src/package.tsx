ddd0d57b75354f0e2d81831dabe9d0757737b109
Adrian Lafond <mail@adrianlafond.com>
2021-04-10 10:34:36 -0400

    create prod/package webpack

---
+++ src/package.tsx
@@ -0,0 +1,2 @@
+export * from './circular-range';
+export * from './horizontal-range';
--- webpack.config.package.js
+++ webpack.config.package.js
@@ -2,6 +2,8 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+  entry: path.join(__dirname, './src/package.tsx'),
+  mode: 'production',
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.json']
   },
@@ -12,9 +14,20 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
         test: /\.tsx?$/,
         loader: 'awesome-typescript-loader'
-      }
-    ]
+      },
+    ],
   },
+  plugins: [
+    new MiniCssExtractPlugin(),
+  ],
 }
